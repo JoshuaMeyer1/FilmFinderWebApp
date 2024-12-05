c5885454278c89a664b15e10b31325d263a6c6b2
JoshuaMeyer1 <97538826+JoshuaMeyer1@users.noreply.github.com>
2024-12-04 23:10:40 -0500

    toastr fixed, testScript removed
    Signed-off-by: JoshuaMeyer1 <97538826+JoshuaMeyer1@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # app.py
 from flask import Flask, request, jsonify, send_from_directory
-from TestScript import get_row_by_index
 from query_metadata import get_metadata_title
 from flask_cors import CORS
 import pandas as pd
@@ -20,22 +19,7 @@ def home():
 def favicon():
     return send_from_directory(directory='./static', path='favicon.ico')
 
-@app.route('/get-row', methods=['POST'])
-def get_row():
-    try:
-        # Extract the index from the request's JSON payload
-        data = request.get_json()
-        index = data.get('index')
 
-        if index is None or not isinstance(index, int):
-            return jsonify({"error": "Invalid index. Please provide an integer."}), 400
-
-        # Call the function from TestScript.py
-        row = get_row_by_index(index)
-
-        return jsonify({"row": row})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 @app.route('/get-movie-details', methods=['POST'])
 def get_movie_details():
@@ -66,3 +50,23 @@ def get_movie_details():
 if __name__ == '__main__':
     app.run(debug=True)
     #app.run(debug=True, host='0.0.0.0')
+
+
+'''
+@app.route('/get-row', methods=['POST'])
+def get_row():
+    try:
+        # Extract the index from the request's JSON payload
+        data = request.get_json()
+        index = data.get('index')
+
+        if index is None or not isinstance(index, int):
+            return jsonify({"error": "Invalid index. Please provide an integer."}), 400
+
+        # Call the function from TestScript.py
+        row = get_row_by_index(index)
+
+        return jsonify({"row": row})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+'''
